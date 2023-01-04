779c80f81a427ed20b17435e500cb98eb27bcf8b
abhishek <ahishekm.21.beis@acharya.ac.in>
2023-01-04 18:29:21 +0530

    Area of a circle

---
+++ areac.cpp
@@ -0,0 +1,13 @@
+// Area of circle
+
+#include <iostream>
+using namespace std;
+#define PI 3.14
+int main()
+{
+    float r;
+    cout<<"Enter the radius:"<<endl;
+    cin>>r;
+    cout<<"Area of circle = "<< PI*r*r;
+    return 0;
+}
