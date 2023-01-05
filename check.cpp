abd339c1d6ff1ca3020bb76e183c7fd55f756d13
abhishek <ahishekm.21.beis@acharya.ac.in>
2023-01-05 18:09:42 +0530

    Postive or Not

---
+++ check.cpp
@@ -0,0 +1,15 @@
+// To check no. is positive or not 
+
+#include<iostream>
+using namespace std;
+int main( )
+    {
+    int n;
+    cout<<"Enter the number: "<<endl;
+    cin>>n;
+    if(n > 0)
+        cout<<"Number is positive"<<endl;
+    if(n < 0)
+        cout<<"Number is negative"<<endl;
+    return 0;
+}
