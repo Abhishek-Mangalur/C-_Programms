15e28a7c7f99f61e357a198e8a36bdcef3f905f5
abhishek <ahishekm.21.beis@acharya.ac.in>
2023-01-07 14:51:25 +0530

    To find n natural no.

---
+++ natural.cpp
@@ -0,0 +1,15 @@
+// To find first n natural no.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<("Enter the n natural no.: ");
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<(i)<<endl;
+    }
+    return 0;
+}
