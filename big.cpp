56adbe09dbb56287e52c54b92c1c464cec8b9af0
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-24 15:29:54 +0530

    Update big.cpp

--- big.cpp
+++ big.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main( )
 {
-    int a, b, big;
+    int a, b, big;  // variable delcaration
     cout<<"Enter the value of a:"<<endl;
-    cin>>a;
+    cin>>a;     // intake of first number
     cout<<"Enter the value of b:"<<endl;
-    cin>>b;
-    big = (a>b) ? a : b;
-    cout<<"The bigger no is: "<<big;
+    cin>>b;     // intake of second number
+    big = (a>b) ? a : b;    // checking which number is big
+    cout<<"The bigger no is: "<<big;    // printing the result
     return 0;
 }
