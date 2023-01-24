cf37c7aa95e37a5bbef7b0df63f47085f66e47e0
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-24 15:35:53 +0530

    Biggest of two number

--- big.cpp
+++ big.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main( )
 {
-    int a, b, big;  // variable delcaration
+    int a, b, big;  // variable delcaration 
     cout<<"Enter the value of a:"<<endl;
     cin>>a;     // intake of first number
     cout<<"Enter the value of b:"<<endl;
