f647f6d21a22403dc5124ff1408312cd30211c14
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-20 18:27:39 +0530

    Addition of two no.

--- addu.cpp
+++ addu.cpp
@@ -1,11 +1,13 @@
+// Addition of two no.
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int a,b,c;
-    cout<<"Enter two numbers:"<<endl;
-    cin>>a>>b;
-    c=a+b;
-    cout<<"The sum is = "<<c;
+    int a,b,c;          // declaration of variables
+    cout << "Enter two numbers: " << endl;
+    cin >> a >> b;          // reading the no.
+    c = a + b;          // calculating exp
+    cout << "The sum is = " << c;       // result
     return 0;
 }
