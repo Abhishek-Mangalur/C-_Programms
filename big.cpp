a3dc64504f46eb18c54691592c4542a91e2f8618
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-23 18:42:31 +0530

    Biggest of two no.

--- big.cpp
+++ big.cpp
@@ -1,15 +1,15 @@
-// Bigger of two numbers
+// Biggest of two numbers
 
 #include<iostream>
 using namespace std;
-int main( )
+int main()
 {
     int a, b, big;  // variable delcaration 
-    cout<<"Enter the value of a:"<<endl;
-    cin>>a;     // intake of first number
-    cout<<"Enter the value of b:"<<endl;
-    cin>>b;     // intake of second number
-    big = (a>b) ? a : b;    // checking which number is big
-    cout<<"The bigger no is: "<<big;    // printing the result
+    cout << "Enter the value of a:"<<endl;
+    cin >> a;     // intake of first number
+    cout << "Enter the value of b:"<<endl;
+    cin >> b;     // intake of second number
+    big = (a > b) ? a : b;    // checking which number is big
+    cout << "The bigger no is: " << big;    // printing the result
     return 0;
 }
