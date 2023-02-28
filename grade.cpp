e2bfb6eb0a7affeae5e1d059bcc821ca9a382a4f
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-28 18:44:27 +0530

    Grading System

--- grade.cpp
+++ grade.cpp
@@ -1,27 +1,28 @@
 // Grading System
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int marks;
-    cout<<("Enter the marks:")<<endl;
-    scanf("%d",&marks);
-    if(marks <= 39)
-        cout<<("Grade F (Fail)")<<endl;
+    int marks;          // declaration of variable
+    cout << "Enter the marks:" << endl;
+    cin >> marks;       // reading marks
+
+    if(marks <= 39)         // condition checking 
+        cout << "Grade F (Fail)" << endl;       // printing result
     else if(marks <= 49)
-        cout<<("Grade E")<<endl;
-    else if(marks <= 59)
-        cout<<("Grade D")<<endl;
+        cout << "Grade E" << endl;
+    else if(marks <= 59)9
+        cout << "Grade D" << endl;
     else if(marks <= 69)
-        cout<<("Grade C")<<endl;
+        cout << "Grade C" << endl;
     else if(marks <= 79)
-        cout<<("Grade B")<<endl;
+        cout << "Grade B" << endl;
     else if(marks <= 89)
-        cout<<("Grade A")<<endl;
+        cout << "Grade A" << endl;
     else if(marks <= 100)
-        cout<<("Grade O (Outstanding)")<<endl;
+        cout << "Grade O (Outstanding)" << endl;
     else
-        cout<<("You have entered more than 100 marks")<<endl;
+        cout << "You have entered more than 100 marks" << endl;     // warning statement
     return 0;
 }
