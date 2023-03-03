aec2d8d8d2aae0bf50467ddb734051d07fde94a3
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-03-03 20:37:01 +0530

    To print 'n' even no.

--- neven.cpp
+++ neven.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main()
 {
-    int i = 2,n;
-    cout<<"Enter the no. upto you want to see: ";
-    cin>>n;
-    while (i<=n)
+    int i = 2, n;       // declaration of variable
+    cout << "Enter the no. upto you want to see: ";
+    cin >> n;       // reading a no.
+    while (i <= n)      // loop
     {
-        cout<<i<<endl;
+        cout << i << endl;      // result printing
         i += 2;
     }
     return 0;
