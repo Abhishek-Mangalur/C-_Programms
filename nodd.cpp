86acdaca770334e944f829d4a6dc468cd3be18e1
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-03-09 18:30:18 +0530

    To print 'n' odd no.

--- nodd.cpp
+++ nodd.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main()
 {
-    int i = 1,n;
-    cout<<"Enter the no. upto you want to see: ";
-    cin>>n;
-    while (i<=n)
+    int i = 1, n;       // declaration of variable
+    cout << "Enter the no. upto you want to see: ";
+    cin >> n;           // reading a no.
+    while (i <= n)      // looping statement
     {
-        cout<<i<<endl;
+        cout << i << endl;      // printing statement
         i += 2;
     }
     return 0;
