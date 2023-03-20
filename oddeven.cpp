3c05416b9eb2f6654a6406f2480f5defaa05b11e
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-03-20 19:12:23 +0530

    To find the no. is odd or even

--- oddeven.cpp
+++ oddeven.cpp
@@ -2,14 +2,15 @@
 
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    int n;
-    cout<<"Enter the number: "<<endl;
-    cin>>n;
-    if(n % 2 == 0)
-        cout<<"Number is even"<<endl;
-    if(n % 2 != 0)
-        cout<<"Number is odd"<<endl;
+    int n;      // declaration of variable
+    cout << "Enter the number: " << endl;
+    cin >> n;           // reading a no.
+    if(n % 2 == 0)      // codition checking 
+        cout << "Number is even" << endl;       // result of even no.
+    else
+        cout << "Number is odd" << endl;
     return 0;
 }
