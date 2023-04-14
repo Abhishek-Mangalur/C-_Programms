426cf5b1d2282a55e8286e5f48a12f90f360a550
Mr. Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-04-14 17:43:09 +0530

    Simple Interest

--- si.cpp
+++ si.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 int main()
 {
-    int a,b,c;
+    int a, b, c;        // variable declaration
     float si;
-    cout<<"Enter the p,t,r:"<<endl;
-    cin>>a>>b>>c;
-    si = (a*b*c)/100;
-    cout<<"Simple Interest is = "<<si;
+    cout << "Enter the p,t,r:" << endl;
+    cin >> a >> b >> c;     // reading three no.s
+    si = (a*b*c)/100;       // calculating simple intrest
+    cout << "Simple Interest is = " << si;      // showing result
     return 0;
 }
