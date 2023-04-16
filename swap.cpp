f98223f681a58b3a2effac8b44ddc294bbb89a9b
Mr. Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-04-16 07:28:40 +0530

    Swap two no. using temp variable

--- swap.cpp
+++ swap.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 int main()
 {
-    int a,b,temp;
-    cout<<"Enter two numbers:"<<endl;
-    cin>>a>>b;
-    cout<<"The numbers before swap: \nnum1 = "<<a<<endl;
-    cout<<"num2 = "<<b<<endl;
-    temp = a;
+    int a, b, temp;         // declaration of variables
+    cout << "Enter two numbers:" << endl;
+    cin >> a >> b;          // reading two no.s
+    cout << "The numbers before swap: \nnum1 = " << a << endl;      
+    cout << "num2 = " << b << endl;
+    temp = a;       // intialize a to temp variable
     a = b;
     b = temp;
-    cout<<"The numbers after swap: \nnum1 = "<<a<<endl;
-    cout<<"num2 = "<<b;
+    cout << "The numbers after swap: \nnum1 = " << a << endl;
+    cout << "num2 = " << b;     // printing result
     return 0;
 }
