f5a4ccf47e765a1b68ddb098be20ba04d82129b3
Mr. Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-04-17 06:51:05 +0530

    Swap two no. without using temp variable

--- swap1.cpp
+++ swap1.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 int main()
 {
-    int a,b;
-    cout<<"Enter two numbers:"<<endl;
-    cin>>a>>b;
-    cout<<"The numbers before swap: \nnum1 = "<<a<<endl;
-    cout<<"num2 = "<<b<<endl;
-    a = a + b;
+    int a, b;           // declaration of variables
+    cout << "Enter two numbers:" << endl;
+    cin >> a >> b;      // reading two no.
+    cout << "The numbers before swap: \nnum1 = " << a << endl;
+    cout << "num2 = " << b << endl;
+    a = a + b;          // swaping technique
     b = a - b;
     a = a - b;
-    cout<<"The numbers after swap: \nnum1 = "<<a<<endl;
-    cout<<"num2 = "<<b<<endl;
+    cout << "The numbers after swap: \nnum1 = " << a << endl;
+    cout << "num2 = " << b << endl;         // printing the result
     return 0;
 }
